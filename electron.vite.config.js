ac22ef8166e43af6f369ae0baa310549045e9859
nktech-official <nk.technical.org@gmail.com>
2023-11-06 09:16:29 +0530

    add cmpas

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,6 +1,10 @@
-import { resolve } from 'node:path'
+import { resolve, join, dirname } from 'node:path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import { createRequire } from 'node:module'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+const require = createRequire(import.meta.url)
+const cMapsDir = join(dirname(require.resolve('pdfjs-dist/package.json')), 'cmaps')
 
 export default defineConfig({
   main: {
@@ -15,6 +19,16 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: cMapsDir,
+            dest: ''
+          }
+        ]
+      })
+    ]
   }
 })
